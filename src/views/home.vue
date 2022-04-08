a9a78e15e53a0dcfe030c0237433686d2238cd8f
lawtedwu <lawtedwu@tencent.com>
2022-04-08 23:02:48 +0800

    add:router Link

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,31 +1,34 @@
 
 <template>
-<n-space>
-  <router-link to="/"> home </router-link>
-  <router-link to="/flow_perception_hefei"> hefei </router-link>
-  <router-link to="/flow_perception_taian"> taian </router-link>
-  <router-link to="flow_2_3"> flow_2_3 </router-link>
-  <router-link to="flow_1_1"> flow_1_1 </router-link>
-  <router-link to="flow_2_1"> flow_2_1 </router-link>
-  <router-link to="flow_2_4"> flow_2_4 </router-link>
-  <router-link to="flow_3_1"> flow_3_1 </router-link>
-  <router-link to="flow_3_2"> flow_3_2 </router-link>
-  <router-link to="flow_3_4"> flow_3_4 </router-link>
-  <router-link to="flow_3_5"> flow_3_5 </router-link>
-  <router-link to="flow_4_1"> flow_4_1 </router-link>
-</n-space>
+  <n-space>
+    <router-link to="/">home</router-link>
+    <n-button type="primary" size="large">
+      <router-link to="/flow_perception_hefei">hefei</router-link>
+    </n-button>
 
+    <router-link to="/flow_perception_taian">taian</router-link>
+    <router-link to="flow_2_3">flow_2_3</router-link>
+    <router-link to="flow_1_1">flow_1_1</router-link>
+    <router-link to="flow_2_1">flow_2_1</router-link>
+    <router-link to="flow_2_4">flow_2_4</router-link>
+    <router-link to="flow_3_1">flow_3_1</router-link>
+    <router-link to="flow_3_2">flow_3_2</router-link>
+    <router-link to="flow_3_4">flow_3_4</router-link>
+    <router-link to="flow_3_5">flow_3_5</router-link>
+    <router-link to="flow_4_1">flow_4_1</router-link>
+  </n-space>
 </template>
 
 <script setup>
-import { NSpace } from 'naive-ui';
+import { NSpace, NButton } from 'naive-ui';
 
 </script>
 
 <style>
 a {
   color: white;
-  font-size: 48px;
+  font-size: 30px;
+  text-decoration: none;
 }
 </style>
 
